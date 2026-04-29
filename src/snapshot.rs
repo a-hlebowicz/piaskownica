780fff685d54f22b6d325d1c65569dd584b611d8
a-hlebowicz <aleksanderhlebowicz@gmail.com>
2026-04-29 19:30:39 +0200

    dodano odczytywanie stanu gry z jsona

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -1,8 +1,6 @@
 use serde::{Serialize, Deserialize};
 use crate::grid::Grid;
 use crate::particle::{CellType, Particle};
-use crate::snapshot;
-
 
 #[derive(Serialize, Deserialize)]
 pub struct Snapshot {
@@ -12,7 +10,7 @@ pub struct Snapshot {
     pub cell_temps: Vec<i16>,
 }
 
-pub fn export(grid: &Grid)  {
+pub fn export(grid: &Grid) -> String {
     let mut cell_types: Vec<String> =Vec::with_capacity(grid.height*grid.width);
     let mut cell_temps: Vec<i16> =Vec::with_capacity(grid.height*grid.width);
     for y in 0..grid.height{
@@ -30,5 +28,28 @@ pub fn export(grid: &Grid)  {
         cell_temps,
     };
 
-    serde_json::to_string(&snapshot).unwrap_or_else(|_| "{}".to_string()); //jak blad to zwroc pusty json
+    serde_json::to_string(&snapshot).unwrap_or_else(|_| "{}".to_string()) //jak blad to zwroc pusty json
+}
+
+pub fn import(grid: &mut Grid, json: &str) -> Result<(), String>{
+    let snapshot: Snapshot =serde_json::from_str(json).map_err(|e| format!("Błąd parsowania JSON: {}", e))?;
+
+    if snapshot.width != grid.width || snapshot.height != grid.height {
+        return Err(format!("niezgodny rozmiar planszy"));
+    }
+    
+    if snapshot.cell_types.len() != grid.width *grid.height || snapshot.cell_temps.len() != grid.width *grid.height {
+        return Err(format!("niezgodna liczba cząstek"))
+    }
+
+    for i in 0..grid.height * grid.width {
+        let cell_type = CellType::from_str(&snapshot.cell_types[i])?;
+        let temperature = snapshot.cell_temps[i]; //&
+        let mut particle = Particle::new_empty();
+        particle.cell_type=cell_type; 
+        particle.temperature=temperature;
+        grid.cells[i] = particle;
+    }
+
+    Ok(())
 }
